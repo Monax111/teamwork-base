6f1b437fb99fbd83bba2eec1ee8c06c693ccddb5
tim <t.p.maerov@tinkoff.ru>
2024-04-24 19:08:43 +0300

    TASK-777 вернул возраст на 18 лет

--- src/main/java/SimpleLogic.java
+++ src/main/java/SimpleLogic.java
@@ -1,7 +1,7 @@
 public class SimpleLogic {
 
     boolean isAdult(Integer age) {
-        if (age < 14) {
+        if (age < 18) {
             System.out.println("Несовершеннолетний клиент");
             return false;
         } else {
