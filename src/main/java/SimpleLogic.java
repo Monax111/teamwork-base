a6b8d336afaa689defaeb082c1e21d06a4e5ee18
d.s.korotchenko <d.s.korotchenko@tinkoff.ru>
2024-04-24 19:08:16 +0300

    TASK-666 Decrease adult age

--- src/main/java/SimpleLogic.java
+++ src/main/java/SimpleLogic.java
@@ -1,7 +1,7 @@
 public class SimpleLogic {
 
     boolean isAdult(Integer age) {
-        if (age < 21) {
+        if (age < 14) {
             System.out.println("Несовершеннолетний клиент");
             return false;
         } else {
